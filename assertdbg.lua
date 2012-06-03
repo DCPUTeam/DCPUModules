def8546ba1fb989d4ad806ac8f5f521b623b71b5
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-06-03 23:48:37 +1000

    Added unit testing modules.

--- assertdbg.lua
+++ assertdbg.lua
@@ -12,9 +12,17 @@ function assert_handler(state, symbol)
     -- handle assertion
     local expr = expression_create(string.sub(symbol, #"assertion:" + 1))
     if (expr:evaluate(resolve_label) ~= 1) then
+      -- attempt to find line information
+      local lineinfo = ""
+      for i, v in ipairs(state:lines()) do
+        if (v.address < state.cpu.registers.pc) then 
+          lineinfo = " after '" .. v.file .. ":" .. v.line .. "'"
+        end
+      end
       -- assertion failed, break
-      print("assertion \"" .. string.sub(symbol, #"assertion:" + 1) .. "\" failed.")
+      print("assertion \"" .. string.sub(symbol, #"assertion:" + 1) .. "\" failed" .. lineinfo .. ".")
       state:_break()
+      state:raise("assertion_failed")
     end
   end
 end
